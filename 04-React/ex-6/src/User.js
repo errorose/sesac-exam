553011498c734eec1ceb2b669b790314845b8429
errorose <yoonyeseul2000@gmail.com>
2024-08-09 01:24:42 +0900

    study: map & filter 실습

--- 04-React/ex-6/src/User.js
+++ 04-React/ex-6/src/User.js
@@ -31,6 +31,26 @@ export default function User() {
         }
     }
 
+    // 키보드 이벤트
+    const handleKeyDown = (e)=>{
+        console.log('keyDown e >> ', e);
+
+        if(e.keyCode === 13){
+            addUser();
+        }
+    }
+
+    // 더블클릭시 삭제
+    const deleteUser = (targetId)=>{
+        console.log('targetId >> ', targetId);
+
+        const newUser = user.filter((user)=>{
+            return user.id !== targetId;
+        })
+        console.log('newUser >> ', newUser);
+        setUser(newUser);
+    }
+
     return(
         <>
             <input 
@@ -48,12 +68,24 @@ export default function User() {
                 onChange={(e)=>{
                     setInputEmail(e.target.value);
                 }}
+                onKeyDown={(e)=>{
+                    handleKeyDown(e);
+                }}
             />
             <button onClick={addUser}>등록</button>
             <div>
                 <ul style={{listStyle: 'none'}}>
                     {user.map((value)=>{
-                        return <li key={value.id}>{value.name}: {value.email}</li>
+                        return (
+                            <li 
+                                key={value.id}
+                                onDoubleClick={()=>{
+                                    deleteUser(value.id);
+                                }}
+                            >
+                                {value.name}: {value.email}
+                            </li>
+                        )
                     })}
                 </ul>
             </div>
